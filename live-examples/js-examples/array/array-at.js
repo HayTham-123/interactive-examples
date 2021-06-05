d65c77abe111cef9aa312b7d7b8ef53221a23d3f
HayTham <69314540+HayTham-123@users.noreply.github.com>
2021-06-05 08:13:47 +0200

    Update array-at.js

--- live-examples/js-examples/array/array-at.js
+++ live-examples/js-examples/array/array-at.js
@@ -1,6 +1,6 @@
 const array1 = [5, 12, 8, 130, 44];
 
-let index = 4;
+let index = 2;
 
 console.log(`Using an index of ${index} the item returned is ${array1.at(index)}`);
 // expected output: "Using an index of 2 the item returned is 8"
